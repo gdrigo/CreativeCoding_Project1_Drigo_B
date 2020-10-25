ff33b7ca3508896fa776c5e55b0f514eb63965d2
gdrigo <gkd244@nyu.edu>
2020-10-25 01:43:33 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,12 +15,12 @@
 function setup() {
   createCanvas(1000,700);
   noCursor();
-	
 }
 
 function draw() {
+	// noStroke();
+	strokeWeight(2); 
 	background(250);
-
 
   	people.push(new Person());
   	for (person in people) { //display all 'people'
@@ -36,70 +36,94 @@ function draw() {
 
 
 class Wall {
-  constructor() {
-  	this.strength = 100; //percentage will relate to its look
-  	this.height = 100;
-  }
+	constructor() {
+	  	this.strength = 100; //percentage will relate to its look
+	  	this.height = 100;
 
-  display() {
-  	noStroke();
-  	fill(0, 10, 100);
-  	rect(0, 150, width, 100);
-  	// textureWall();
-  }
+	  	this.bricks = [];
+	  	for (var xPos = 0; xPos < 1100; xPos+=100) {
+	  		for (var yPos = 150; yPos < 250; yPos+=20) {
+	  			if(yPos == 170 || yPos == 210 || yPos == 250) {
+	  				this.bricks.push(new Brick([xPos-50, yPos]));
+	  			}
+	  			else {
+	  				this.bricks.push(new Brick([xPos, yPos]));
+	  			}
+	  		}
+	  	}
+	  }
 
-  textureWall() {
-  	fill(90, 20, 39);
-  	for(let i = 0; i < 3333; i++) { //adds small lines to wall to add texture
-	  	let x1 = Math.random() * this.height;
-	    let y1 = Math.random() * this.width;
-	    let theta = Math.random() * 2 * Math.PI;
-	    let segmentLength = Math.random() * 5 + 2;
-	    let x2 = Math.cos(theta) * segmentLength + x1;
-	    let y2 = Math.sin(theta) * segmentLength + y1;
-	   	line(x1, y1, x2, y2);
-	}
-    stroke(20,20 - Math.random() * 5,20 - Math.random() * 8, Math.random() * 10 + 75);
-  }
 
-  weaken() {}
+  	display() {
+	  	// noStroke();
+	  	// fill(0, 10, 100);
+	  	// rect(0, 150, width, 100);
+	  	for (var brick = 0; brick < this.bricks.length; brick++) {
+	  		this.bricks[brick].display();
+	  	}
+  	}
 
-  move() {}
+  	brickCollision(x,y,rad) {
+  		//checks which brick was hit by looking at position of main figure
+  		let personTop = y - rad/2;
+  		let personBottom = y + rad/2;
+  		let personLeft = x - rad/2;
+  		let personRight = x + rad/2;
 
-  openWall()  {}
+  		for (var i = 0; i < this.bricks.length; i++) {
+  			let brickTop = this.bricks[i].pos[1];
+	  		let brickBottom = this.bricks[i].pos[1] + this.bricks[i].height;
+	  		let brickLeft = this.bricks[i].pos[0]-rad/2;
+	  		let brickRight = this.bricks[i].pos[0] + this.bricks[i].width+rad/2;
+
+	  		console.log(personLeft, brickLeft);
+
+	  		if (personTop == brickBottom && (personLeft >= brickLeft && personRight <= brickRight )) {
+	  			this.bricks[i].weaken();
+	  			console.log("TOP");
+	  		}
+
+
+	  		// if (personBottom == brickTop) {
+	  		// 	this.bricks[i].weaken();
+	  		// 	console.log("HIT");
+	  		// }
+	  		// if (personLeft == brickRight) {
+	  		// 	this.bricks[i].weaken();
+	  		// 	console.log("L");
+	  		// }
+	  		// if (personRight == brickLeft) {
+	  		// 	this.bricks[i].weaken();
+	  		// }
+  			
+	  		// this.bricks[brick].display();
+	  	}
+  	}
+
 }
+
+
 class Brick {
   constructor(pos) {
+  	this.pos = pos;
   	this.strength = 100; //percentage will relate to its look
-  	this.height = 100;
+  	this.height = 20;
+  	this.width = 100;
+  	// this.col = []
   }
 
   display() {
-  	noStroke();
-  	fill(0, 10, 100);
-  	rect(0, 150, width, 100);
-  	// textureWall();
+  	// strokeWeight(2); 
+  	fill(245, 221, 150, map(this.strength,0,100,20,255));
+  	rect(this.pos[0], this.pos[1], this.width, this.height);
   }
 
-  textureWall() {
-  	fill(90, 20, 39);
-  	for(let i = 0; i < 3333; i++) { //adds small lines to wall to add texture
-	  	let x1 = Math.random() * this.height;
-	    let y1 = Math.random() * this.width;
-	    let theta = Math.random() * 2 * Math.PI;
-	    let segmentLength = Math.random() * 5 + 2;
-	    let x2 = Math.cos(theta) * segmentLength + x1;
-	    let y2 = Math.sin(theta) * segmentLength + y1;
-	   	line(x1, y1, x2, y2);
-	}
-    stroke(20,20 - Math.random() * 5,20 - Math.random() * 8, Math.random() * 10 + 75);
+  weaken() {
+  	this.strength-=0.3;
   }
 
-  weaken() {}
+  fall() {}
 
-  move() {}
-
-  openWall()  {}
 }
 
 class Person {
@@ -113,11 +137,19 @@ class Person {
 		this.col = col;
 		// this.vel = new createVector(0, Math.floor((Math.random() * 50) + 10) );
 		this.vel = vel;
+		this.maxHeight = 250+35;
+
+		//levels represnet how much of the wall has been broken
+		this.levelOne = false;
+		this.levelTwo = false;
+		this.levelThree = false;
+		this.levelFour = false;
+		this.levelFive = false;
+
 		
 	}
 
 	display() {
-		// this.move();
 		fill(this.col[0],this.col[1],this.col[2], this.col[3]);
 		ellipse(this.pos[0], this.pos[1], this.rad, this.rad);
 	}
@@ -127,14 +159,42 @@ class Person {
 		this.pos[1]-=this.vel[1];
 	}
 
+
+	///////////////////////
 	//only for 'mainPerson'
 	updatePos() {
+		if(!this.levelOne && !this.levelTwo && !this.levelThree && !this.levelFour && !this.levelFive) { //hasnt broken any blocks
+
+		}
+		else {
+			if(this.levelOne) {
+
+			}
+			if(this.levelTwo) {
+
+			}
+			if(this.levelThree) {
+
+			}
+			if(this.levelFour) {
+
+			}
+			if(this.levelFive) {
+
+			}
+		}
+		
+
 		this.pos[0] = mouseX; 
-		this.pos[1] = constrain(mouseY, 250+35, height-35)
+		this.pos[1] = constrain(mouseY, this.maxHeight, height-35);
+
+		if (this.pos[1] == this.maxHeight) {
+			wall.brickCollision(this.pos[0],this.pos[1],this.rad);
+			// console.log("HIT DIFFERENT", this.pos[0], this.pos[1]);
+		}
+
 		// let m = map(this.pos[0], 0, 800, 250+35, height);
 	}
-
-	getWidth() {}
 }
 
 
@@ -145,17 +205,6 @@ var mainPerson = new Person([500,800-100], [0,0], 70, [242,34,63,255]);
 var people = [];
 
 
-// // void mouseClicked() {
-// //   currColor++;
-// //   if (currColor == colors.length) {
-// //     currColor = 0;
-// //   }
-// // }
 
-// // void fullShape(float centerX, float centerY, float size) {
-// //   drawTriangle(centerX-size/2,centerY+(size*0.267), size); //left triangle
-// //   drawTriangle(centerX+size/2, centerY+(size*0.267), size); //right triangle
-// //   drawTriangle(centerX, centerY-size*0.86+(size*0.267), size); // triangle
-// // }
 
 
